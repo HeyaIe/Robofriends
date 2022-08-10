43f409d131f11f912377a9d611defc4ec25547df
HeyaIe <98930139+HeyaIe@users.noreply.github.com>
2022-08-10 18:34:12 -0400

    Initial commit

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Bryan',
+        username: 'Bry',
+        email: 'bryngx@gmail.com'
+    },
+    {
+        id: 2,
+        name: 'Heyale',
+        username: 'Ale',
+        email: 'heyale@softooei.com'
+    },
+    {
+        id: 3,
+        name: 'Amberlin',
+        username: 'Am',
+        email: 'amberlin@ggex.com'
+    },
+    {
+        id: 4,
+        name: 'Victoria',
+        username: 'Vicky',
+        email: 'Vicky@eg.com'
+    }
+]
